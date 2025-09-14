a56dd88fb940d81e0333d89a6e4bf1e63c745a3e
Barry Nelson <abcbarryn@users.noreply.github.com>
2025-09-14 03:32:01 -0400

    Add files via upload

---
+++ telnetd/CMakeFiles/in.telnetd.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/usr/src/packages/BUILD/telnet-ssl-0.17.42/telnetd/global.c" "telnetd/CMakeFiles/in.telnetd.dir/global.c.o" "gcc" "telnetd/CMakeFiles/in.telnetd.dir/global.c.o.d"
+  "/usr/src/packages/BUILD/telnet-ssl-0.17.42/telnetd/setproctitle.c" "telnetd/CMakeFiles/in.telnetd.dir/setproctitle.c.o" "gcc" "telnetd/CMakeFiles/in.telnetd.dir/setproctitle.c.o.d"
+  "/usr/src/packages/BUILD/telnet-ssl-0.17.42/telnetd/slc.c" "telnetd/CMakeFiles/in.telnetd.dir/slc.c.o" "gcc" "telnetd/CMakeFiles/in.telnetd.dir/slc.c.o.d"
+  "/usr/src/packages/BUILD/telnet-ssl-0.17.42/telnetd/state.c" "telnetd/CMakeFiles/in.telnetd.dir/state.c.o" "gcc" "telnetd/CMakeFiles/in.telnetd.dir/state.c.o.d"
+  "/usr/src/packages/BUILD/telnet-ssl-0.17.42/telnetd/sys_term.c" "telnetd/CMakeFiles/in.telnetd.dir/sys_term.c.o" "gcc" "telnetd/CMakeFiles/in.telnetd.dir/sys_term.c.o.d"
+  "/usr/src/packages/BUILD/telnet-ssl-0.17.42/telnetd/telnetd.c" "telnetd/CMakeFiles/in.telnetd.dir/telnetd.c.o" "gcc" "telnetd/CMakeFiles/in.telnetd.dir/telnetd.c.o.d"
+  "/usr/src/packages/BUILD/telnet-ssl-0.17.42/telnetd/termstat.c" "telnetd/CMakeFiles/in.telnetd.dir/termstat.c.o" "gcc" "telnetd/CMakeFiles/in.telnetd.dir/termstat.c.o.d"
+  "/usr/src/packages/BUILD/telnet-ssl-0.17.42/telnetd/utility.c" "telnetd/CMakeFiles/in.telnetd.dir/utility.c.o" "gcc" "telnetd/CMakeFiles/in.telnetd.dir/utility.c.o.d"
+  "" "telnetd/in.telnetd" "gcc" "telnetd/CMakeFiles/in.telnetd.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ telnetd/CMakeFiles/in.telnetd.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/in.telnetd.dir/link.d"
+  "CMakeFiles/in.telnetd.dir/global.c.o"
+  "CMakeFiles/in.telnetd.dir/global.c.o.d"
+  "CMakeFiles/in.telnetd.dir/setproctitle.c.o"
+  "CMakeFiles/in.telnetd.dir/setproctitle.c.o.d"
+  "CMakeFiles/in.telnetd.dir/slc.c.o"
+  "CMakeFiles/in.telnetd.dir/slc.c.o.d"
+  "CMakeFiles/in.telnetd.dir/state.c.o"
+  "CMakeFiles/in.telnetd.dir/state.c.o.d"
+  "CMakeFiles/in.telnetd.dir/sys_term.c.o"
+  "CMakeFiles/in.telnetd.dir/sys_term.c.o.d"
+  "CMakeFiles/in.telnetd.dir/telnetd.c.o"
+  "CMakeFiles/in.telnetd.dir/telnetd.c.o.d"
+  "CMakeFiles/in.telnetd.dir/termstat.c.o"
+  "CMakeFiles/in.telnetd.dir/termstat.c.o.d"
+  "CMakeFiles/in.telnetd.dir/utility.c.o"
+  "CMakeFiles/in.telnetd.dir/utility.c.o.d"
+  "in.telnetd"
+  "in.telnetd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/in.telnetd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
