d16b7472a3c026a477a366ce849de28216b8ed4b
Barry Nelson <abcbarryn@users.noreply.github.com>
2025-09-14 03:33:21 -0400

    Add files via upload

---
+++ telnetlogin/CMakeFiles/telnetlogin.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/telnetlogin.dir/link.d"
+  "CMakeFiles/telnetlogin.dir/telnetlogin.c.o"
+  "CMakeFiles/telnetlogin.dir/telnetlogin.c.o.d"
+  "telnetlogin"
+  "telnetlogin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/telnetlogin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ telnetlogin/CMakeFiles/telnetlogin.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for telnetlogin.
+# This may be replaced when dependencies are built.
